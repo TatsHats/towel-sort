0fe19c5c944fa5ba5d5255d8545a1190e9eff305
TatsHats <6642017@gmail.com>
2024-08-07 18:59:10 +0300

    feat: modified file index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+// ------ Сортировка полотенец ------
+// Разбиваем матрицу по рядам
+// переворачиваем каждый четный ряд 
+// объединяем ряды в один массив
 
-// You should implement your task here.
+function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
 
-module.exports = function towelSort (matrix) {
-  return [];
+  return matrix.map((row, index) => {
+      if (index % 2 === 0) {
+          return row; // Четный ряд
+      } else {
+          return row.reverse(); // Нечетный ряд: переворачиваем строку
+      }
+  }).flat(); // Объединяем
 }
+
+module.exports = towelSort;
